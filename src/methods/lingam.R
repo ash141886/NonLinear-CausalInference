c53ff2be851cf2877c399ffe931591be656c6540
Islam Md Ashraful <100204628+ash141886@users.noreply.github.com>
2025-02-08 12:06:22 +0900

    Create lingam.R

---
+++ src/methods/lingam.R
@@ -0,0 +1,21 @@
+library(fastICA)
+
+run_lingam_algorithm <- function(data) {
+    start_time <- Sys.time()
+    res <- tryCatch({
+        ica_result <- fastICA(data, n.comp = ncol(data))
+        W <- ica_result$W
+        B <- solve(W) %*% diag(apply(W, 2, max))
+        diag(B) <- 0
+        B[abs(B) < 0.01] <- 0
+        end_time <- Sys.time()
+        time_taken <- as.numeric(difftime(end_time, start_time, units = "secs"))
+        list(dag = B != 0, time = time_taken)
+    }, error = function(e) {
+        cat("Error in LiNGAM:", e$message, "\n")
+        end_time <- Sys.time()
+        time_taken <- as.numeric(difftime(end_time, start_time, units = "secs"))
+        list(dag = matrix(0, ncol(data), ncol(data)), time = time_taken)
+    })
+    res
+}
