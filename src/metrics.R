6423a70de59b732acb983d2511832ed9b33f485e
Islam Md Ashraful <100204628+ash141886@users.noreply.github.com>
2025-06-26 15:34:35 +0900

    metrics.R

--- src/metrics.R
+++ src/metrics.R
@@ -38,3 +38,41 @@ calculate_dag_metrics <- function(estimated_dag, true_dag) {
         SHD = SHD, MSE = MSE, MAE = MAE
     )
 }
+
+# -------------------------------------------
+# Simulation (single experiment)
+# -------------------------------------------
+analyze_causal_structure <- function(n_vars, n_samples, nonlinearity = 0.3,
+                                     sparsity = 0.3, noise_level = 0.1,
+                                     gam_sigma = 1, threshold_percentile = 0.3) {
+    data <- generate_data(n_vars, n_samples, nonlinearity, sparsity, noise_level)
+    true_dag <- matrix(0, n_vars, n_vars)
+    true_dag[upper.tri(true_dag)] <- 1   # assuming topological order
+    methods <- c("Proposed Method", "LiNGAM")
+    results <- data.frame(Method = methods, Time = NA, stringsAsFactors = FALSE)
+    for (method in methods) {
+        start_time <- Sys.time()
+        if (method == "Proposed Method") {
+            dag <- tryCatch({
+                run_proposed_method(data, sigma = gam_sigma, threshold_percentile = threshold_percentile)
+            }, error = function(e) matrix(0, n_vars, n_vars))
+            time_taken <- as.numeric(difftime(Sys.time(), start_time, units = "secs"))
+        } else if (method == "LiNGAM") {
+            lingam_result <- run_lingam_algorithm(data)
+            dag <- lingam_result$dag
+            time_taken <- lingam_result$time
+        }
+        results$Time[results$Method == method] <- time_taken
+        metrics <- calculate_dag_metrics(dag, true_dag)
+        metrics[is.na(metrics)] <- 0
+        results[results$Method == method, names(metrics)] <- metrics
+    }
+    results$Variables <- n_vars
+    results$Samples <- n_samples
+    results$Nonlinearity <- nonlinearity
+    results$Sparsity <- sparsity
+    results$Noise <- noise_level
+    results$Threshold <- threshold_percentile
+    results$gam_sigma <- gam_sigma
+    results
+}
