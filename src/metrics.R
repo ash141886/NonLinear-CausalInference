87bae0103626322209ddfeea1f54251c190e3bb4
Islam Md Ashraful <100204628+ash141886@users.noreply.github.com>
2025-06-26 12:15:06 +0900

    metrics.R

--- src/metrics.R
+++ src/metrics.R
@@ -1,37 +1,40 @@
-# Your existing calculate_dag_metrics_improved function
-calculate_dag_metrics_improved <- function(estimated_dag, true_dag) {
+# -------------------------------------------
+# DAG Metric Calculation
+# -------------------------------------------
+calculate_dag_metrics <- function(estimated_dag, true_dag) {
     n <- nrow(true_dag)
-    TP_dir <- 0; FP_dir <- 0; FN_dir <- 0; misoriented <- 0; TN_dir <- 0
+    TP <- FP <- FN <- misoriented <- TN <- 0
     for (i in 1:n) {
         for (j in 1:n) {
             if (i == j) next
             if (true_dag[i, j] == 1) {
                 if (estimated_dag[i, j] == 1) {
-                    TP_dir <- TP_dir + 1
+                    TP <- TP + 1
                 } else if (estimated_dag[j, i] == 1) {
                     misoriented <- misoriented + 1
-                    FN_dir <- FN_dir + 1
+                    FN <- FN + 1
                 } else {
-                    FN_dir <- FN_dir + 1
+                    FN <- FN + 1
                 }
             } else {
                 if (estimated_dag[i, j] == 1) {
-                    FP_dir <- FP_dir + 1
+                    FP <- FP + 1
                 } else {
-                    TN_dir <- TN_dir + 1
+                    TN <- TN + 1
                 }
             }
         }
     }
-    Precision_dir <- ifelse((TP_dir + FP_dir) > 0, TP_dir / (TP_dir + FP_dir), 0)
-    Recall_dir <- ifelse((TP_dir + FN_dir) > 0, TP_dir / (TP_dir + FN_dir), 0)
-    F1_Score_dir <- ifelse((Precision_dir + Recall_dir) > 0,
-                           2 * Precision_dir * Recall_dir / (Precision_dir + Recall_dir), 0)
-    Graph_Accuracy <- (TP_dir + TN_dir) / (n * (n - 1))
+    Precision <- ifelse((TP + FP) > 0, TP / (TP + FP), 0)
+    Recall <- ifelse((TP + FN) > 0, TP / (TP + FN), 0)
+    F1 <- ifelse((Precision + Recall) > 0, 2 * Precision * Recall / (Precision + Recall), 0)
+    Accuracy <- (TP + TN) / (n * (n - 1))
     SHD <- sum(abs(estimated_dag - true_dag))
     MSE <- mean((true_dag - estimated_dag)[row(true_dag) != col(true_dag)]^2)
     MAE <- mean(abs(true_dag - estimated_dag)[row(true_dag) != col(true_dag)])
-    c(Precision_dir = Precision_dir, Recall_dir = Recall_dir, F1_Score_dir = F1_Score_dir,
-      Graph_Accuracy = Graph_Accuracy, Misoriented = misoriented, SHD = SHD,
-      MSE = MSE, MAE = MAE)
+    c(
+        Precision = Precision, Recall = Recall, F1 = F1, 
+        Accuracy = Accuracy, Misoriented = misoriented,
+        SHD = SHD, MSE = MSE, MAE = MAE
+    )
 }
